a2ea522de3258b6fbe3b49bfb957ddb711318923
root <root@localhost.localdomain>
2019-12-29 21:10:35 +0100

    init project

---
+++ view/template/template.js
@@ -0,0 +1,13 @@
+var myIndex = 0;
+function diapo() {
+    var i;
+    var x = document.getElementsByClassName("slide");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(diapo, 3000); // Change image every chaque 3s
+}
+
